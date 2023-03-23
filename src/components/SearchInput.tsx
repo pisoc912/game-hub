962d48ac00edc601e3cb0fe4e243925603eb22be
syf <shenyf1124@outlook.com>
2023-03-23 15:26:10 -0700

    Build Search Input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,19 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React from "react";
+import { BsSearch } from "react-icons/bs";
+
+const SearchInput = () => {
+  return (
+    <InputGroup>
+      <InputLeftElement children={<BsSearch />} />
+      <Input
+        borderRadius={20}
+        placeholder="Search Games.."
+        variant="filled"
+        autoComplete=""
+      />
+    </InputGroup>
+  );
+};
+
+export default SearchInput;
